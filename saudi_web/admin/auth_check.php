0fdbdb910052581f677aca28bb6522b28f931750
NoufAbdullahS <444200547@student.ksu.edu.sa>
2026-05-08 23:55:14 +0300

    Delete saudi_web directory

--- saudi_web/admin/auth_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Include this at the top of every admin page to protect it
-session_start();
-
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: login.php");
-    exit;
-}
-?>
